a0f8582cceb68259840c6f43960e87d936c9aab3
Mr-RedHat-fb <mrredhat317@gmail.com>
2025-11-29 19:19:46 +0100

    fixed config and hooks and settings

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-import 'dotenv/config';
+// src/config.js
+import "dotenv/config";
 
 export default {
-  webhook: process.env.DISCORD_WEBHOOK_URL,
-  token: process.env.FLASHBACK_TOKEN,
-  interval: Number(process.env.POLL_INTERVAL) || 5000,
-  flashbackUrl: process.env.FLASHBACK_FORUM_URL
+  discordWebhook: process.env.DISCORD_WEBHOOK_URL
 };
-
