d7305795661590ae9de4bdaeffcac7e9cb557cc1
Mr-RedHat-fb <mrredhat317@gmail.com>
2025-11-29 18:47:40 +0100

    runtime error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (args.includes("--nu")) {
   console.log("Watchern startar…");
   setInterval(runWatcher, 5000);
 } else if (args.includes("--inte")) {
-  console.log("Watchern stoppas (fast egentligen gör du inget här).");
+  console.log("Watchern stoppas.");
   process.exit(0);
 } else {
   console.log("Använd: knarka --nu   eller   knarka --inte");
